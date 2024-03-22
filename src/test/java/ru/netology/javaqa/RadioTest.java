3acda0093a1f60da256a4f0468822b83c566c1e6
Alexandr Shchelokov <alex175171@gmail.com>
2024-03-22 15:23:09 +0300

    updated tests, lombok

--- src/test/java/ru/netology/javaqa/RadioTest.java
+++ src/test/java/ru/netology/javaqa/RadioTest.java
@@ -16,38 +16,67 @@ public class RadioTest {
     }
 
     @Test
-    public void shouldCreateTestCase2() {
+    public void shouldSetCurrentRadioStationVolume() {
         Radio radio = new Radio();
 
-        radio.setNumberOfStations(30);
+        radio.setCurrentSoundVolume(88);
 
-        Assertions.assertEquals(30, radio.getNumberOfStations());
+        Assertions.assertEquals(88, radio.getCurrentSoundVolume());
     }
 
     @Test
-    public void shouldCreateTestCase3() {
+    public void shouldGetCurrentRadioStationVolume() {
         Radio radio = new Radio();
 
-        radio.setMinStation(0);
+        Assertions.assertEquals(0, radio.getCurrentSoundVolume());
+    }
+
+    @Test
+    public void shouldGetRadioStationNumbers() {
+        Radio radio = new Radio();
+
+        Assertions.assertEquals(10, radio.getNumberOfStations());
+    }
+
+    @Test
+    public void shouldGetMinRadioStationNumber() {
+        Radio radio = new Radio();
 
         Assertions.assertEquals(0, radio.getMinStation());
     }
 
     @Test
-    public void shouldCreateTestCase4() {
+    public void shouldGetCurrentRadioStationNumber() {
         Radio radio = new Radio();
 
-        radio.setCurrentRadioStationNumber(15);
-
-        Assertions.assertEquals(15, radio.getCurrentRadioStationNumber());
+        Assertions.assertEquals(0, radio.getCurrentRadioStationNumber());
     }
 
     @Test
-    public void shouldCreateTestCase5() {
+    public void shouldSetNumberOfStations() {
         Radio radio = new Radio();
 
-        radio.setCurrentSoundVolume(100);
+        radio.setNumberOfStations(55);
 
-        Assertions.assertEquals(100, radio.getCurrentSoundVolume());
+        Assertions.assertEquals(55, radio.getNumberOfStations());
+
+    }
+
+    @Test
+    public void shouldSetMinStation() {
+        Radio radio = new Radio();
+
+        radio.setMinStation(15);
+
+        Assertions.assertEquals(15, radio.getMinStation());
+    }
+
+    @Test
+    public void shouldSetCurrentRadioStation() {
+        Radio radio = new Radio();
+
+        radio.setCurrentRadioStationNumber(8);
+
+        Assertions.assertEquals(8, radio.getCurrentRadioStationNumber());
     }
 }
